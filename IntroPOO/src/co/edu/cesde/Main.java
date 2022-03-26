c3b57424d59283452f2dca455d7918f0104c9600
Juan Zabala <juanzabaladev@gmail.com>
2022-03-25 20:33:31 -0500

    constructor de la clase

--- IntroPOO/src/co/edu/cesde/Main.java
+++ IntroPOO/src/co/edu/cesde/Main.java
@@ -1,9 +1,24 @@
 package co.edu.cesde;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner inputData = new Scanner(System.in);
+        String productName, brand;
+        double price;
+        System.out.println("Ingrese el nombre del producto");
+        productName = inputData.nextLine();
+        System.out.println("Ingrese el precio");
+        price = Double.parseDouble(inputData.nextLine());
+        System.out.println("Ingrese la marca");
+        brand = inputData.nextLine();
+        Product monitorHp = new Product(productName, price, brand);
+        String name = monitorHp.getName();
+        System.out.println(name);
+        System.out.println(monitorHp.getPrice());
         Carro tesla = new Carro();
         //tesla.color = "Negro";
         tesla.encender();
@@ -12,5 +27,6 @@ public class Main {
         tesla.setColor("verde");
         tesla.showColor();
         String color = tesla.getColor();
+
     }
 }
---
+++ IntroPOO/src/co/edu/cesde/Product.java
@@ -0,0 +1,59 @@
+package co.edu.cesde;
+
+public class Product {
+
+    private String color, reference, name, brand, model;
+    double price;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product(String name, double price, String brand) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
