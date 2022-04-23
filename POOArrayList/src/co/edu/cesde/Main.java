35f7cc0f55e5deb30e88b6422f766db01e7a871a
Juan Zabala <juanzabaladev@gmail.com>
2022-04-22 20:26:43 -0500

    list data

---
+++ POOArrayList/src/co/edu/cesde/Main.java
@@ -0,0 +1,73 @@
+package co.edu.cesde;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Student> students = new ArrayList<>();
+        String stopExecution = "NO";
+        while (stopExecution.equals("NO")){
+            System.out.println("ingrese el nombre el estudiante");
+            Student student = new Student();
+            student.setName(scanner.nextLine());
+            System.out.println("Ingrese el documento");
+            student.setIdentification(scanner.nextLine());
+            students.add(student);
+            System.out.println("Desea continuar -SI - NO");
+            String stop = scanner.nextLine();
+            if(stop.equalsIgnoreCase("NO")){
+                break;
+            }
+        }
+        // muestra la lista
+        for(int i=0; i<students.size(); i++){
+            System.out.println("Nombre: " + students.get(i).getName());
+        }
+
+        // Busca estudiante
+        System.out.println("Ingrese el nombre del estudiante a buscar");
+        String name = scanner.nextLine();
+        for (int i=0; i<students.size(); i++){
+            if(students.get(i).getName().equalsIgnoreCase(name)){
+                System.out.println("El estudiante: " +
+                        name + "su identificación es: " +
+                        students.get(i).getIdentification());
+                break;
+            }
+        }
+
+        // Elimina estudiante
+        System.out.println("Ingrese el nombre del estudiante a buscar");
+        String identification = scanner.nextLine();
+        for (int i=0; i<students.size(); i++){
+            if(students.get(i).getIdentification().equalsIgnoreCase(identification)){
+                students.remove(i);
+                break;
+            }
+        }
+        for(int i=0; i<students.size(); i++){
+            System.out.println("Nombre: " + students.get(i).getName());
+        }
+
+        // Actualiza estudiante
+        System.out.println("Ingrese la identificación del estudiante a buscar");
+        String id = scanner.nextLine();
+        for (int i=0; i<students.size(); i++){
+            if(students.get(i).getIdentification().equalsIgnoreCase(id)){
+                Student student = students.get(i);
+                System.out.println("Igrese la nueva identificación");
+                String ident = scanner.nextLine();
+                student.setIdentification(ident);
+                students.set(i, student);
+                break;
+            }
+        }
+        for(int i=0; i<students.size(); i++){
+            System.out.println("Nombre: " + students.get(i).getName());
+        }
+    }
+}
---
+++ POOArrayList/src/co/edu/cesde/Student.java
@@ -0,0 +1,31 @@
+package co.edu.cesde;
+
+public class Student {
+    private String photo;
+    private String name;
+    private String identification;
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+}
